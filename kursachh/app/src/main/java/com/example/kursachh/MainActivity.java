0da2cb0178adc30bce3afe9763a405244f2a7389
Asya <135468933+axionyy@users.noreply.github.com>
2024-12-09 13:38:20 +0300

    1

---
+++ kursachh/app/src/main/java/com/example/kursachh/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.kursachh;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization);
+
+    }
+
+    public void EnterAutorization(View v) {
+        Intent intent = new Intent(MainActivity.this, NavigationRun.class);
+        startActivity(intent);
+    }
+
+    public void Registration(View v) {
+        Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ kursachh/app/src/main/java/com/example/kursachh/ui/profile/ProfileFragment.java
@@ -0,0 +1,35 @@
+package com.example.kursachh.ui.profile;
+
+import androidx.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kursachh.R;
+
+public class ProfileFragment extends Fragment {
+
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
+    }
+
+    private ProfileViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
